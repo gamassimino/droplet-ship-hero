6ef7c9de2469b43663607e111277206db1d69fa3
Tad Thorley <phaedryx@gmail.com>
2025-03-20 13:43:02 -0600

    add Fluid rubocop rules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  protected
+protected
 
   def after_sign_in_path_for(resource)
     admin_dashboard_index_path
